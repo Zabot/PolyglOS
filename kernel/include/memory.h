f8f67df9111cb3562706de46caf9d459cd46c14f
Zach Anderson <zach.inbox@gmail.com>
2022-03-22 21:16:25 -0500

    Initialize memory frame bitmap

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -1,6 +1,11 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#define PAGE_SIZE 4096 * 1024
+
 void printMemory();
+void initalizeFrameBitmap();
+int getFrame();
+void printFrames();
 
 #endif
--- kernel/src/memory.c
+++ kernel/src/memory.c
@@ -1,12 +1,11 @@
+#include "memory.h"
+
 #include <stdint.h>
 
 #include "io/format.h"
+#include "bitmap.h"
 
-#include "memory.h"
-
-#define VIRTUAL_MEMORY_SIZE 0xFFFFFFFF
-#define PAGE_SIZE 4096
-#define PAGE_TABLE_DEPTH 2
+#define BIOS_MEMORY_TYPE_USABLE 1
 
 /*Type 1: Usable (normal) RAM*/
 /*Type 2: Reserved - unusable*/
@@ -25,9 +24,68 @@ extern uint32_t memoryMapLength;
 extern struct MemoryMapEntry memoryMap[];
 
 void printMemory() {
-  int mapLength = (memoryMapLength - (uint32_t)(&memoryMap))/24;
   printf("System memory map\nStart:Length (Type)\n----------------\n");
+  int mapLength = (memoryMapLength - (uint32_t)(&memoryMap))/24;
   for (int i = 0; i < mapLength; i++) {
-    printf("%x:%x (%x)\n", memoryMap[i].baseAddress, memoryMap[i].length, memoryMap[i].type);
+    printf("%l:%l (%d)\n", memoryMap[i].baseAddress, memoryMap[i].baseAddress + memoryMap[i].length - 1, memoryMap[i].type);
   }
+}
+
+int frames;
+WORD *framesInUse;
+void initalizeFrameBitmap() {
+  // Determine the number of memory frames
+  uint64_t max = 0;
+  int mapLength = (memoryMapLength - (uint32_t)(&memoryMap))/24;
+  for (int i = 0; i < mapLength; i++) {
+    if (memoryMap[i].baseAddress + memoryMap[i].length > max)
+      max = memoryMap[i].baseAddress + memoryMap[i].length;
+  }
+  frames = max / PAGE_SIZE;
+
+  // In order to bootstrap memory managment we need to slap this bitmap
+  // somewhere. Find a suitable chunk of memory
+  int bitmap_size = frames / WORD_SIZE;
+  // TODO Read through memory map looking for something that has enough memory
+  // Assign a voidpointer to framesInUse
+  framesInUse = (void *)(WORD)memoryMap[3].baseAddress;
+
+  // The frames bitmap is initalized to have no unused frames. Then we loop
+  // through the memory map twice. The first time is to free any frames that the
+  // memory map reports are usable. The second time is to handle the case of
+  // overlapping entries allowing some unusable memory to appear usable by
+  // explicitly removing any memory that is unusable.
+  // TODO We need to put this somewhere
+  setBitmap(framesInUse, 0, frames, 1);
+  for (int b = 0; b < 2; b++) {
+    for (int i = 0; i < mapLength; i++) {
+      int baseFrame = memoryMap[i].baseAddress / PAGE_SIZE;
+      int endFrame = (memoryMap[i].baseAddress + memoryMap[i].length) / PAGE_SIZE;
+      int length = endFrame - baseFrame + 1;
+
+      if (b == 0 && memoryMap[i].type == BIOS_MEMORY_TYPE_USABLE)
+        setBitmap(framesInUse, baseFrame, length, 0);
+      if (b == 1 && memoryMap[i].type != BIOS_MEMORY_TYPE_USABLE)
+        setBitmap(framesInUse, baseFrame, length, 1);
+    }
+  }
+}
+
+int getFrame() {
+  for (int f = 0; f < frames; f++) {
+    if (getBitmap(framesInUse, f) == 0) {
+      setBitmap(framesInUse, f, 1, 1);
+      return f;
+    }
+  }
+
+  return -1;
+}
+
+void printFrames() {
+  for (int f = 0; f < frames; f++) {
+    printf("%d", getBitmap(framesInUse, f));
+  }
+  printf("\n");
+  printf("Dumped %d frames\n", frames);
 }
