f7b71c2d3260cc5b9eb5f54dd727f4b1df2740bc
Zach Anderson <zach.inbox@gmail.com>
2022-03-23 19:44:04 -0500

    Reserve frames used by the memory map

--- kernel/src/memory.c
+++ kernel/src/memory.c
@@ -37,6 +37,8 @@ void initalizeFrameBitmap() {
   // Determine the number of memory frames
   uint64_t max = 0;
   int mapLength = (memoryMapLength - (uint32_t)(&memoryMap))/24;
+  // TODO Flatten the memory map list so we don't have to deal with overlapping
+  //      regions elsewhere.
   for (int i = 0; i < mapLength; i++) {
     if (memoryMap[i].baseAddress + memoryMap[i].length > max)
       max = memoryMap[i].baseAddress + memoryMap[i].length;
@@ -48,7 +50,6 @@ void initalizeFrameBitmap() {
   // somewhere. Find a suitable chunk of memory in the memory map
   int bitmap_size = frames / WORD_SIZE;
   for (int i = 0; i < mapLength; i++) {
-    // TODO This doesn't handle overlapping memory regions
     if (memoryMap[i].type == BIOS_MEMORY_TYPE_USABLE) {
       if (memoryMap[i].length > bitmap_size) {
         // Set framesInUse to the very end of the last addressable region. Using
@@ -62,23 +63,36 @@ void initalizeFrameBitmap() {
   printf("Storing memory bitmap at %x\n", framesInUse);
 
   // The frames bitmap is initalized to have no unused frames. Then we loop
-  // through the memory map twice. The first time is to free any frames that the
-  // memory map reports are usable. The second time is to handle the case of
-  // overlapping entries allowing some unusable memory to appear usable by
-  // explicitly removing any memory that is unusable.
+  // through the memory map to free an usable frames
+  printf("Initalizing memory bitmap...\n");
   setBitmap(framesInUse, 0, frames, 1);
-  for (int b = 0; b < 2; b++) {
-    for (int i = 0; i < mapLength; i++) {
-      int baseFrame = memoryMap[i].baseAddress / PAGE_SIZE;
-      int endFrame = (memoryMap[i].baseAddress + memoryMap[i].length) / PAGE_SIZE;
-      int length = endFrame - baseFrame + 1;
+  printf("Marking free memory frames...\n");
+  for (int i = 0; i < mapLength; i++) {
+    // Since regions may start and stop off of frame borders, we need to calculate
+    // the start and stop frame of each region.
+    int baseFrame = memoryMap[i].baseAddress / PAGE_SIZE;
+    int endFrame = (memoryMap[i].baseAddress + memoryMap[i].length) / PAGE_SIZE;
+    int length = endFrame - baseFrame;
 
-      if (b == 0 && memoryMap[i].type == BIOS_MEMORY_TYPE_USABLE)
-        setBitmap(framesInUse, baseFrame, length, 0);
-      if (b == 1 && memoryMap[i].type != BIOS_MEMORY_TYPE_USABLE)
-        setBitmap(framesInUse, baseFrame, length, 1);
+    // We add an additional frame of padding because the base frame is rounded
+    // down, and may therefore contain unusable memory
+    if (memoryMap[i].type == BIOS_MEMORY_TYPE_USABLE) {
+      printf("Freeing frames %x to %x...\n", baseFrame, endFrame);
+      setBitmap(framesInUse, baseFrame + 1, length - 1, 0);
     }
   }
+
+  // Allocate pages for memory bitmap
+  int memoryMapStartFrame = (int)framesInUse / PAGE_SIZE;
+  int memoryMapFrames  = (bitmap_size / PAGE_SIZE + 1);
+  printf("Reserving frames %x to %x for memory bitmap...\n", memoryMapStartFrame, memoryMapFrames);
+  setBitmap(framesInUse, memoryMapStartFrame, memoryMapFrames, 1);
+
+  // TODO Allocate pages in use by the kernel
+
+  // The memory bitmap now resides in memory at the top of the address space
+  // and is initalized with only the frames reserved by the BIOS, and the frames
+  // containing the memory bitmap itself.
 }
 
 int getFrame() {
